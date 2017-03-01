7dfc1a625346aae9327f7f224a6c1fc7dc475781
Adam Shannon <adamkshannon@gmail.com>
2017-03-01 13:18:12 -0600

    format: failing test for multiple playlists in the same file

--- format/reader_test.go
+++ format/reader_test.go
@@ -1,7 +1,6 @@
 package format
 
 import (
-	"fmt"
 	"github.com/adamdecaf/univplayfmt"
 	"testing"
 )
@@ -54,7 +53,44 @@ entries:
 			t.Errorf("parsed example playlist doesn't match answer, parsed=%v", out)
 		}
 	}
+}
 
-	fmt.Println(out[0])
-	fmt.Println(answer)
+func TestParse__MultiplePlaylists(t *testing.T) {
+	in := `---
+format: UPL1
+name: Favorites
+id: 2b43009f-d6a6-4f00-8533-09a9a73d8b54
+entries:
+- artist: Anciients
+  title: Following the Voice
+  duration: 408.764081632
+  ids:
+    sha2: e577cce68a69735acccd5d8603b3e663f6aa5bc9
+    sha3: e577cce68a69735acccd5d8603b3e663f6aa5bc9
+    mbtrackid: b00a2b97-53f1-485a-9121-1fe76b55e651
+    filepath: Anciients/Following the Voice.mp3
+
+format: UPL1
+name: Least Favorites
+id: 49db4841-43f3-4f8d-9467-6443fa4ca1de
+entries:
+- artist: U2
+  title: Vertigo
+  duration: 1.100
+  ids:
+    sha2:
+    sha3: e577cce68a69735acccd5d8603b3e663f6aa5bc9
+    mbtrackid: c00a2b97-53f1-485a-9121-1fe76b55e651
+    filepath: U2/Vertigo.mp3
+`
+	out, err := readString(in)
+	if err != nil {
+		t.Errorf("error parsing example: err=%v", err)
+	}
+
+	// just skip it for now, error when we fix this
+	if len(out) != 2 {
+		t.Skip("multiple playlist parsing in the same file isn't supported right now")
+	}
+	t.Error("multiple playlist parsing in the same file is fixed")
 }
